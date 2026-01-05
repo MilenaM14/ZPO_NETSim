a474080958be62e8b619d8e41c374733c72242cc
jowitamaciejny <jowitamaciejny@gmail.com>
2026-01-05 22:26:52 +0100

    poprawki

--- src/package.cpp
+++ src/package.cpp
@@ -1,17 +1,3 @@
 //
 // Created by milen on 14.12.2025.
 //
-#include <iostream>
-class IPackageStockPile
-{
-public:
-    using const_iterator = std::list<Package>::const_iterator;
-    virtual~IPackageStockPile() = default;
-    virtual void push(const Package&) = 0;
-    virtual std::size_t size() const = 0;
-    virtual bool empty() const = 0;
-    virtual const_iterator begin() = 0;
-    virtual const_iterator end()  = 0;
-    virtual const_iterator cbegin() const = 0;
-    virtual const_iterator cend() const = 0;
-}
