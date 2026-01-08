66eced5f2ef5c9661418569b66f4d5f9af648bc0
jowitamaciejny <jowitamaciejny@gmail.com>
2026-01-08 21:28:51 +0100

    dodanie interfejsu, klas receiverpreferences i package sender

--- include/nodes_preferences.hpp
+++ include/nodes_preferences.hpp
@@ -5,4 +5,73 @@
 #ifndef NODES_PREFERENCES_HPP
 #define NODES_PREFERENCES_HPP
 
+#include "helpers.hpp"
+#include "package.hpp"
+#include "storage_types.hpp"
+#include "types.hpp"
+#include <map>
+#include <optional>
+
+enum class ReceiverType {
+    WORKER,
+    STOREHOUSE
+}; //informacje o obiekcie
+class IPackageReceiver
+{
+public:
+    virtual IPackageStockpile::const_iterator cbegin() const = 0;
+
+    virtual IPackageStockpile::const_iterator cend() const = 0;
+
+    virtual IPackageStockpile::const_iterator begin() const = 0;
+
+    virtual IPackageStockpile::const_iterator end() const = 0;
+
+    virtual ReceiverType get_receiver_type() const = 0; //identyfikacja typu obiektu
+
+    virtual void receive_package(Package&& p) = 0; //przekazuje wskaźnik
+
+    virtual ElementID get_id() const = 0; //identyfikacja ID
+
+    virtual ~IPackageReceiver() = default; //destruktor
+};
+class ReceiverPreferences
+{
+public:
+    using preferences_t = std::map<IPackageReceiver*, double>; //alias na typ kontenera
+    using const_iterator = preferences_t::const_iterator; //alias na typ kontenera
+    const_iterator begin() const;
+    const_iterator end() const;
+    const_iterator cbegin() const;
+    const_iterator cend() const;
+    explicit ReceiverPreferences(ProbabilityGenerator pg = probability_generator);
+
+    void add_receiver(IPackageReceiver* r); // metoda która dodaje nowego odbiorce
+    void remove_receiver(IPackageReceiver* r); //metora, która usuwa odbiorce
+
+    IPackageReceiver* choose_receiver();
+
+    const preferences_t& get_preferences() const;
+
+private:
+    preferences_t preferences_;
+    ProbabilityGenerator pg_;
+};
+
+class PackageSender {
+public:
+    ReceiverPreferences receiver_preferences_;
+
+    PackageSender(PackageSender&&) = default;  // konstruktor przenoszący (domyślny)
+
+    void send_package();
+    const std::optional<Package>& get_sending_buffer() const { return buffer_; };
+
+private:
+    void push_package(Package&& p) {
+        buffer_ = std::move(p);
+    }
+    std::optional<Package> buffer_ = std::nullopt;
+};
+
 #endif //NODES_PREFERENCES_HPP
--- src/nodes_preferences.cpp
+++ src/nodes_preferences.cpp
@@ -1,3 +1,44 @@
 //
 // Created by jowit on 08.01.2026.
 //
+#include "nodes_preferences.hpp"
+
+ReceiverPreferences::ReceiverPreferences(ProbabilityGenerator pg)
+    : pg_(pg) {
+} // Zapis generatora do pola klasy
+
+void ReceiverPreferences::add_receiver(IPackageReceiver* r)
+{
+    preferences_[r] = 0.0;
+    std::size_t n = preferences_.size(); //ilość odbiorców
+    for (auto& receiver : preferences_) {
+        receiver.second = 1.0/n; //przypisnie wartosci
+    }
+}
+
+void ReceiverPreferences::remove_receiver(IPackageReceiver* r) {
+    preferences_.erase(r); //usuwa odbiorce
+    if (preferences_.empty()) return;
+
+    double p = 1.0 / preferences_.size(); //nowa wartosc po usunieciu odbiorcy
+
+    for (auto& receiver : preferences_) {
+        receiver.second = p; // przypisanie nowych wartosci
+    }
+}
+
+IPackageReceiver* ReceiverPreferences::choose_receiver() {
+    if (preferences_.empty()) return nullptr; //żeby nie iterowac po pustej mapie
+
+    auto p = pg_(); // losuje liczbę z [0,1]
+    double cumulative = 0.0;
+
+    for (auto& receiver : preferences_) {
+        cumulative += receiver.second;
+        if (p <= cumulative) {
+            return receiver.first; // wybrany odbiorca
+        }
+    }
+
+    return preferences_.rbegin()->first;
+}
