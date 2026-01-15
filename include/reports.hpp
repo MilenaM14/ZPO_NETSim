99417be13101b16c253e0ca32865fee130e7ae08
Milena <milenamelaniuk@gmail.com>
2026-01-15 19:21:30 +0100

    dodanie wszytskiego

--- include/reports.hpp
+++ include/reports.hpp
@@ -5,4 +5,13 @@
 #ifndef AAANETSIM_REPORTS_HPP
 #define AAANETSIM_REPORTS_HPP
 
+#include "factory.hpp"
+#include <iostream>
+
+//generuje raport o strukturze sieci
+void generate_structure_report(const Factory& f, std::ostream& os);
+
+//generuje raport o stanie symulacji w danej turze
+void generate_simulation_turn_report(const Factory& f, std::ostream& os, Time t);
+
 #endif //AAANETSIM_REPORTS_HPP
